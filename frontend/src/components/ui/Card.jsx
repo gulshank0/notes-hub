f8bdb1d1a8a838f866920fe8ad22632794b53fdc
gulshank0 <gulshanbahadur002@gmail.com>
2025-03-21 23:55:27 +0530

    Card components adde

--- frontend/src/components/ui/Card.jsx
+++ frontend/src/components/ui/Card.jsx
@@ -1,7 +1,7 @@
 
 export function Card(props) {
   return (
-  <div className="bg-red-600 rounded-2xl text-center " >
+  <div className="bg-red-600 rounded-2xl flex justify-center items-center  " >
     <h1>{props.title}</h1> 
   </div>
   
