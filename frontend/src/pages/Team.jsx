5688e1526c4c0e0778d4c79dd5ee9ce4b73415d8
gulshank0 <gulshanbahadur002@gmail.com>
2025-03-25 12:00:31 +0530

    improved team page

--- frontend/src/pages/Team.jsx
+++ frontend/src/pages/Team.jsx
@@ -7,28 +7,30 @@ function TeamPage() {
       <Navbar />
       <div className="min-h-screen bg-black ">
         <div className="pt-30 flex justify-center">
-          <div className=" bg-gradient-to-br from-blue-500 to-purple-500 rounded-4xl py-30 px-30 flex flex-col hover:bg-transparent cursor-pointer transition delay-150 duration-100 ease-in-out hover:translate-y-1 hover:scale-110 ">
+          <div className=" bg-gradient-to-br from-blue-500 to-purple-500 rounded-4xl py-30 px-30 flex flex-col hover:bg-transparent transition delay-150 duration-100 ease-in-out hover:translate-y-1 hover:scale-110 ">
             <div className=" rounded-full flex justify-center pb-5 ">
               <img
                 src="../../public/CuteCat.png"
                 alt="Description"
-                className="w-40 h-40 object-cover rounded-full"
+                className="w-40 h-40 object-cover rounded-full shadow-amber-200 hover:cursor-pointer"
               />
             </div>
             <p className="text-3xl text-center">Linux Guy</p>
-            <p>
-              {" "}
+            <p className="text-center">
               Full Stack Developer <br /> Using Arch btw
             </p>
-            <div className="flex flex-col gap-2">
-              <a>
-                <Twitter />
+            <div className="flex space-x-4 pt-3 justify-center">
+              <a href="https://x.com/gulshank0" target="_blank">
+                <Twitter className="cursor-pointer" />
               </a>
-              <a>
-                <GithubIcon />
+              <a href="https://github.com/gulshank0" target="_blank">
+                <GithubIcon className="cursor-pointer" />
               </a>
-              <a>
-                <LinkedinIcon />
+              <a
+                href="https://www.linkedin.com/in/gulshan-kumar-872512270/"
+                target="_blank"
+              >
+                <LinkedinIcon className="cursor-pointer" />
               </a>
             </div>
           </div>
