b958ac9c2b0a55f20797e68dfd9fd77a6bf77cac
gulshank0 <gulshanbahadur002@gmail.com>
2025-03-29 08:31:26 +0530

    improved ui in syllabus page

--- frontend/src/pages/Syllabus.jsx
+++ frontend/src/pages/Syllabus.jsx
@@ -5,10 +5,10 @@ function Syllabus() {
   return (
     <div>
       <Navbar />
-      <div className="h-screen flex justify-center bg-black text-5xl text-white pt-35">
+      <div className="h-screen flex justify-center bg-black text-5xl text-white pt-0">
         <div className="bg-gradient-to-br from-blue-500 to-red-500 rounded-2xl px-80 m-20 ">
           <p className="text-6xl text-white pb-6 ">Syllabus</p>
-          <div className="flex flex-col items-center gap-20">
+          <div className="flex flex-col items-center gap-30">
             <p>unit-1 </p>
             <p>unit-2 </p>
             <p>unit-3 </p>
