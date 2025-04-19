ce67298bd68a47c6e26b820f19be48588b3f7398
gulshank0 <gulshanbahadur002@gmail.com>
2025-04-19 12:31:18 +0530

    auth routes modified

--- be/src/routes/authRoutes.js
+++ be/src/routes/authRoutes.js
@@ -1,7 +1,8 @@
 import express from "express";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
+import prisma from "../prismaClient";
 
-
-const router =express();
-
+const router = express.Router();
+//
+//// Register new User End point
